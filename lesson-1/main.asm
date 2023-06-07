81352853b6c056d6a61c96f15a0a623d1e7a0990
Toggy Smith <toggysmith@gmail.com>
2023-06-07 17:08:34 +0100

    chore: add some comments to lesson 1

--- lesson-1/main.asm
+++ lesson-1/main.asm
@@ -8,5 +8,8 @@ section .text
     mov edx, 13
     mov ecx, msg
     mov ebx, 1
-    mov eax, 4
+    ; `eax` stores system call number
+    mov eax, 4 ; 4 = sys_write
+    int 80h ; request software interrupt
+    mov eax, 1 ; 1 = sys_exit
     int 80h
