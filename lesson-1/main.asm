d9ec0d3f8672dc5f8ec1e70f1d73b3e5d41a6bb9
Toggy Smith <toggysmith@gmail.com>
2023-06-07 17:16:22 +0100

    refactor: change `int 80h` to `int 0x80`

--- lesson-1/main.asm
+++ lesson-1/main.asm
@@ -10,6 +10,6 @@ section .text
     mov ebx, 1
     ; `eax` stores system call number
     mov eax, 4 ; 4 = sys_write
-    int 80h ; request software interrupt
+    int 0x80 ; request software interrupt
     mov eax, 1 ; 1 = sys_exit
-    int 80h
+    int 0x80 
